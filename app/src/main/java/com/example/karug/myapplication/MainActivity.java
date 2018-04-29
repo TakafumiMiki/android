3b24be24a57ea6e76febd90d77b1a4bb5356e78f
TakafumiMiki <karugami02468@gmail.com>
2018-04-29 21:17:08 +0900

    pushし忘れ

--- app/src/main/java/com/example/karug/myapplication/MainActivity.java
+++ app/src/main/java/com/example/karug/myapplication/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.karug.myapplication;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
